64895c06566b9989a59b416296930aa3a4e5ec28
Mario Arias <carlosmario293@gmail.com>
2022-01-10 08:36:36 -0500

    New NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const NavBar = () => {
+
+
+    return (
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="col-1"></div>
+            <div>
+                <a href="/">
+
+                </a>
+            </div>
+            <a class="navbar-brand" href="/">  Film Store</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="/">Home</a>
+                    <a class="nav-item nav-link" href="/">Acerca de nosotros</a>
+                    <a class="nav-item nav-link" href="/">Productos y Servicios</a>
+                </div>
+            </div>
+        </nav>
+
+
+
+    )
+
+}
+
+
+
+export default NavBar;
